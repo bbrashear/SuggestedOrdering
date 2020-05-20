0ff9a4789a0018e6453145b9c973931af55ce020
Blake Brashear <bbrashear94@gmail.com>
2020-05-20 14:03:48 -0400

    Added connection string info to appsettings.json, Added appsettings.json to .gitignore, Created Product Model and added properties

---
+++ SuggestedOrdering/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuggestedOrdering.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public int ItemNumber { get; set; }
+        public string Description { get; set; }
+        public string CaseSize { get; set; }
+        public int UsageNumber { get; set; }
+        public int OnHand { get; set; }
+    }
+}
