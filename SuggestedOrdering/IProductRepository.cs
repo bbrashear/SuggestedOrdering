7a7fe9b942edf16710bf6038072e8b149b5833d6
Blake Brashear <bbrashear94@gmail.com>
2020-05-20 14:30:33 -0400

    Created IProductRepository w/ GetAllProducts method, Created ProductRepository that implements interface

---
+++ SuggestedOrdering/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SuggestedOrdering.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuggestedOrdering
+{
+    interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+    }
+}
